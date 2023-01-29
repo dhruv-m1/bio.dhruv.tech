b146bd14408ee4b2a1fd420674c5a603352694c7
Dhruv <26849655+dhruv-tech@users.noreply.github.com>
2023-01-29 05:34:46 +0530

    beta ready to deploy

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,6 +9,7 @@ module.exports = {
 				'medium-dark-blue': 'hsla(221, 55%, 11%, 1)',
 				'medium-blue': 'hsla(221, 58%, 13%, 1)',
 				'light-blue': 'hsla(198, 87%, 42%, 1)',
+				'medium-turquoise': 'hsla(191, 86%, 41%, 1)',
 				'turquoise-25': 'hsla(191, 86%, 41%, 0.25)',
 				'turquoise-6': 'hsla(191, 86%, 41%, 0.06)',
 				'turquoise-0': 'hsla(191, 86%, 41%, 0)',
