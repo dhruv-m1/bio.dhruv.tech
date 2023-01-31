87535d2394f330eafca4146ed80bb46127544ede
Dhruv <26849655+dhruv-tech@users.noreply.github.com>
2023-01-31 18:10:23 +0530

    improve compression

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 import { defineConfig } from 'astro/config';
-
 import lottie from "astro-integration-lottie";
-
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
+import compressor from "astro-compressor";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [lottie(), tailwind({config: { applyBaseStyles: false }})]
+  integrations: [lottie(), tailwind({
+    config: {
+      applyBaseStyles: false
+    }
+  }), compressor()]
 });
