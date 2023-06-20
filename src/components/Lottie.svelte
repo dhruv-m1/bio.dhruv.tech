e61ca010bb967fcbc70bc355477dc220d1db07f4
Dhruv Malik <26849655+dhruv-tech@users.noreply.github.com>
2023-06-20 13:32:53 -0700

    improved core vitals

---
+++ src/components/Lottie.svelte
@@ -0,0 +1,28 @@
+
+<script type='text/partytown'>
+
+    import { onMount } from 'svelte';
+    import { v4 as uuidv4 } from 'uuid';
+
+    export let autoplay = 'true';
+    export let loop = 'true';
+    export let mode = 'normal';
+    export let src;
+    let identifier = `lottie-${uuidv4()}`;
+
+    onMount(async () => {
+		console.log(identifier);
+        const player = document.querySelector(`#${identifier}`);
+        player.load(`${src}.lottie`);
+	});
+    
+    
+</script>
+    
+<dotlottie-player
+    id={identifier}
+    autoplay={autoplay}
+    loop={loop}
+    mode={mode}
+>
+</dotlottie-player>
