8f39adeae380f3ecc90bd76f69ff9aadf91dcff4
Dhruv Malik <26849655+dhruv-tech@users.noreply.github.com>
2023-10-13 23:09:18 -0700

    accessiblity - fix colour contrast, remove i tags

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,7 +8,7 @@ module.exports = {
 				'dark-blue': 'hsla(223, 49%, 8%, 1)',
 				'medium-dark-blue': 'hsla(221, 55%, 11%, 1)',
 				'medium-blue': 'hsla(221, 58%, 13%, 1)',
-				'light-blue': 'hsla(198, 87%, 42%, 1)',
+				'light-blue': 'hsla(198, 94%, 36%, 1)',
 				'medium-turquoise': 'hsla(191, 86%, 41%, 1)',
 				'turquoise-25': 'hsla(191, 86%, 41%, 0.25)',
 				'turquoise-6': 'hsla(191, 86%, 41%, 0.06)',
